ee79e47793dcebe432089ce8f58fa0eec3daf192
ShubhranshuSanjeev <shubhranshu.sanjeev@gmail.com>
2025-10-07 06:34:57 +0530

    attack lab

--- user/attack.c
+++ user/attack.c
@@ -1,13 +1,14 @@
-#include "kernel/types.h"
-#include "kernel/fcntl.h"
-#include "user/user.h"
-#include "kernel/riscv.h"
+#include "../kernel/types.h"
+#include "../kernel/fcntl.h"
+#include "../user/user.h"
+#include "../kernel/riscv.h"
 
 int
 main(int argc, char *argv[])
 {
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
+  // got this address by memory inspection -- not sure how this value came
+  char *secret = sbrk(PGSIZE*9);
+  secret = secret + 8 * PGSIZE + 16;
 
-  exit(1);
+  write(2, secret, 8);
 }
