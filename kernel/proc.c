350fce0a5866292a9fc5124fedc35857ea33a18c
ShubhranshuSanjeev <shubhranshu.sanjeev@gmail.com>
2025-10-15 18:27:35 +0530

    speed up system call lab

--- kernel/proc.c
+++ kernel/proc.c
@@ -202,6 +202,15 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  struct usyscall* usys = kalloc();
+  usys->pid = p->pid;
+  if(mappages(pagetable, USYSCALL, PGSIZE, (uint64)usys, PTE_U | PTE_R) < 0) {
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -212,6 +221,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
